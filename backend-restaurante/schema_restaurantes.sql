09483265eb34f071727e6d31791c41ea31eb35f6
Dany Bautista <danyjavierb@gmail.com>
2021-07-03 09:02:30 -0500

    creacion db sql 2

--- backend-restaurante/schema_restaurantes.sql
+++ backend-restaurante/schema_restaurantes.sql
@@ -1,26 +1,3 @@
--- MySQL Workbench Forward Engineering
-
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-
--- -----------------------------------------------------
--- Schema restaurante
--- -----------------------------------------------------
-
--- -----------------------------------------------------
--- Schema restaurante
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `restaurante` DEFAULT CHARACTER SET utf8 ;
--- -----------------------------------------------------
--- Schema restaurante
--- -----------------------------------------------------
-
--- -----------------------------------------------------
--- Schema restaurante
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `restaurante` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
-USE `restaurante` ;
 
 -- -----------------------------------------------------
 -- Table `restaurante`.`platos`
@@ -111,14 +88,6 @@ CREATE TABLE IF NOT EXISTS `restaurante`.`pedidos` (
 ENGINE = InnoDB;
 
 
--- -----------------------------------------------------
--- Table `restaurante`.`esta`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `restaurante`.`esta` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
 
 -- -----------------------------------------------------
 -- Table `restaurante`.`pedidos_has_platos`
@@ -142,8 +111,3 @@ CREATE TABLE IF NOT EXISTS `restaurante`.`pedidos_has_platos` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-USE `restaurante` ;
-
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
