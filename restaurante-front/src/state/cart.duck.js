dc052675e85a2816af121a8e58f1eb5982733a40
Dany Bautista <danyjavierb@gmail.com>
2021-07-24 10:11:53 -0500

    agregar items carrito unidad redcuer

--- restaurante-front/src/state/cart.duck.js
+++ restaurante-front/src/state/cart.duck.js
@@ -1,8 +1,17 @@
 const ADD_CART_SUCCESS_TYPE = "ADD_CART_SUCCESS";
+const ADD_UNIT_TYPE = "ADD_UNIT";
 
 export const addToCart = (cartItem) => (dispatch) => {
   dispatch({
     type: ADD_CART_SUCCESS_TYPE,
+    payload: cartItem,
+  });
+  return Promise.resolve();
+};
+
+export const addUnit = (cartItem) => (dispatch) => {
+  dispatch({
+    type: ADD_UNIT_TYPE,
     payload: cartItem,
   });
   return Promise.resolve();
@@ -14,7 +23,8 @@ export default function (state = [], action) {
   switch (type) {
     case ADD_CART_SUCCESS_TYPE:
       return state.concat(payload);
-
+    case ADD_UNIT_TYPE:
+      return; // magiiiiaaa
     default:
       return state;
   }
